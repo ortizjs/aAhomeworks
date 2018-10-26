a6f28b9525d19cb25749ab5e5e97da72e68d0b25
Jonnatan Ortiz <ortizjs@uci.edu>
2018-10-25 23:36:36 -0700

    Add Stack class

---
+++ W1D5/stack.rb
@@ -0,0 +1,19 @@
+class Stack
+
+  def initialize
+    @stack = []
+
+  end
+
+  def push(ele)
+    @stack.push(ele)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    return @stack.last
+  end
+end
