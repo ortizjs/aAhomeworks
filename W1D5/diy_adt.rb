899055de4690b8c93d411007fd98adba62fef83a
Jonnatan Ortiz <jonnatano94@gmail.com>
2018-10-26 07:26:12 -0700

    Add Map class

--- W1D5/diy_adt.rb
+++ W1D5/diy_adt.rb
@@ -38,3 +38,45 @@ class Queue
     @queue.first
   end
 end
+
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def set(key, value)
+    pair_idx = @map.index {|pair| pair[0] == key}
+    if pair_idx
+      @map[pair_idx][1] = value
+    else
+      @map.push([key, value])
+    end
+    value
+  end
+
+  def get(key, value)
+    @map.each do |sub_arr|
+      sub_arr.each do |k, v|
+        return v if k == key
+      end
+    end
+  end
+
+  def delete(key)
+    @map.each do |sub_arr|
+      if sub_arr.include?(key)
+        @map.reject! {|pair| pair[0] == key}
+      end
+    end
+    get(key)
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  def deep_dup(arr)
+    arr.map { |ele| ele.is_a?(Arry) ? deep_dup(ele) : ele }
+  end
+end
