086a48dab3b1fa8647bbd156de9b841497fe6d9e
Jonnatan Ortiz <jonnatano94@gmail.com>
2018-12-02 21:09:07 -0800

    Commit for W6D5 and W7D1 Homework

---
+++ W7D1/job_listing/frontend/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+  city: "Please Select", 
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SWITCH_LOCATION":
+      return {
+        city: action.city,
+        jobs: action.jobs
+      };
+    default:
+      return state;
+  }
+};
+
+// reducer(null, action);
+window.reducer = reducer;
+
+export default reducer;
